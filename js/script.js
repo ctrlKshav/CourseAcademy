e742a448d5d4b81441d9a212bf6a3992195006ba
Keshav <keshavm9978@gmail.com>
2024-03-28 18:28:16 +0530

    Completed Till FAQ's

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-﻿
+﻿//Anonymous function to change navbar color during scroll
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('windowScroll',window.scrollY>0)
+})
+
+//Show or Hide Faq's
+function toggleFunc(faq){
+}
+const faqs=document.querySelectorAll('.faq');
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open')
+
+        //change icon
+        const icon=faq.querySelector('.faq_icon i');
+        if(icon.className==='uil uil-angle-down'){
+            icon.className='uil uil-minus'
+        }
+        else{
+            icon.className='uil uil-angle-down'
+        }
+
+
+    })
+})
